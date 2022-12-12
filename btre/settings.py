568383350ce33344bfcf01902f15e74bdec3b602
sourav kumar <91384229+4s00r@users.noreply.github.com>
2022-12-12 21:31:46 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -156,8 +156,8 @@ MESSAGE_TAGS = {
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'email4mdjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'wjqebjcbeeaobrxw'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS=True
 
 
