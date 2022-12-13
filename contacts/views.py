a943faec95b01dc8caa09dea3803a250a5549a9f
sourav kumar <91384229+4s00r@users.noreply.github.com>
2022-12-13 20:34:37 +0530

    Update views.py

--- contacts/views.py
+++ contacts/views.py
@@ -30,8 +30,8 @@ def contact(request):
     send_mail(
       'Property Listing Inquiry', 
       'There has been an inquiry for ' + listing + '. sign into the admin panel for more info',
-      'email4mdjango@gmail.com',
-      [realtor_email,'souravk2512@gmail.com'],
+      'fromtest@test.com',
+      [realtor_email],
       fail_silently = False
     )
 
